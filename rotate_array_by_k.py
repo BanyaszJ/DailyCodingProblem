1f2a5c0b72790dd7c9c4e1d6682e84fec529a942
BanyaszJ <banyasz.joco@gmail.hu>
2023-02-11 21:18:32 +0100

    Added description of problem

--- rotate_array_by_k.py
+++ rotate_array_by_k.py
@@ -1,3 +1,7 @@
+# This problem was asked by Amazon.
+# Given an array and a number k that's smaller than the length of the array, 
+# rotate the array to the right k elements in-place.
+
 l = [1,"a",2,"b",3,"c"]
 # l = [1,2,3,4,5,6,7,8,9,10]
 # l = [1,None,3,4,5,6,7,8,None,10]
@@ -5,5 +9,5 @@ l = [1,"a",2,"b",3,"c"]
 k = 3
 for i in range(k):       
    l.insert(0, l.pop())
-   
+
 print(l)
